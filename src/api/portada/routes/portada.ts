c5f38bccecb8176de1298acf1aeede77f77ab780
jrodriguez-mcbrokers <jrodriguez@mcbrokers.com.mx>
2025-12-16 13:49:29 -0600

    first commit

---
+++ src/api/portada/routes/portada.ts
@@ -0,0 +1,7 @@
+/**
+ * portada router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::portada.portada');
---
+++ src/api/portada/services/portada.ts
@@ -0,0 +1,7 @@
+/**
+ * portada service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::portada.portada');
