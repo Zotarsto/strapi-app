e134509217f5f5208e4ff5574aa1f837df44407a
jrodriguez-mcbrokers <jrodriguez@mcbrokers.com.mx>
2025-12-19 12:13:17 -0600

    env

---
+++ dist/src/api/asset/controllers/asset.js
@@ -0,0 +1,7 @@
+"use strict";
+/**
+ * asset controller
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const strapi_1 = require("@strapi/strapi");
+exports.default = strapi_1.factories.createCoreController('api::asset.asset');
